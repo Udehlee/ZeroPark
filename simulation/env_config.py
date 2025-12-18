30451428018349d2ca3b6ca159f97b0e6f89ad36
udehlee <ritzeepita@gmail.com>
2025-12-18 21:09:10 +0100

    feat: add domain randomization for parking environment

--- simulation/env_config.py
+++ simulation/env_config.py
@@ -1,36 +1,70 @@
 import airsim
 import time
+import numpy as np
+import os
+from env_randomizer import Env_Randomizer
 
-# connect to the AirSim simulator
+os.makedirs("data", exist_ok=True)
+
+ENV_CONFIG = {
+    "max_obstacles": 3,
+    "sim_area_bounds": ([-30, -30, 0], [30, 30, 0]),
+}
+
+EPISODES = 5
+STEPS_PER_EPISODE = 50
+STEP_TIME = 0.2
+
+# connect to AirSim
 client = airsim.CarClient()
 client.confirmConnection()
 client.enableApiControl(True)
+
 car_controls = airsim.CarControls()
+env = Env_Randomizer(client, ENV_CONFIG)
 
-while True:
-    # get state of the car
-    car_state = client.getCarState()
-    print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
-
-    # set the controls for car
-    car_controls.throttle = 1
-    car_controls.steering = 1
-    client.setCarControls(car_controls)
-
-    # let car drive a bit
+print(" Connected to AirSim")
+for episode in range(EPISODES):
+    print(f" EPISODE {episode + 1}")
+  
+    client.reset()
+    client.enableApiControl(True)
     time.sleep(1)
 
-    # get camera images from the car
-    responses = client.simGetImages([
-        airsim.ImageRequest(0, airsim.ImageType.DepthVis),
-        airsim.ImageRequest(1, airsim.ImageType.DepthPlanar, True)])
-    print('Retrieved images: %d', len(responses))
+    env.random_lighting()
+    parking_slot = env.random_parking_slot()
+    obstacles = env.random_obstacles()
 
-    # do something with images
-    for response in responses:
-        if response.pixels_as_float:
-            print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
-            airsim.write_pfm('py1.pfm', airsim.get_pfm_array(response))
-        else:
-            print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
-            airsim.write_file('py1.png', response.image_data_uint8)
+    print(f"Parking Slot Pose: {parking_slot['pose']}")
+    collided = False
+
+    for step in range(STEPS_PER_EPISODE):
+
+         # set the controls for car
+        car_controls.throttle = 0.6
+        car_controls.steering = np.sin(step * 0.1)
+        client.setCarControls(car_controls)
+
+        # get state of the car
+        state = client.getCarState()
+
+        collision = client.simGetCollisionInfo() 
+
+        print(f"[Episode {episode+1} - Step {step}] " f"Speed={state.speed:.2f} - "f"Collision={collision.has_collided}")
+        if collision.has_collided:
+            print(" Collision detected: ending episode")
+            collided = True
+            break
+
+       # get camera images from the car
+        responses = client.simGetImages([airsim.ImageRequest(0, airsim.ImageType.Scene)])
+        if responses and responses[0].image_data_uint8:
+            filename = f"data/ep{episode}_step{step}.png"
+            airsim.write_file(filename, responses[0].image_data_uint8)
+
+        time.sleep(STEP_TIME)
+
+    print(f"Episode {episode + 1} finished - "f"Collision: {collided}")
+
+client.enableApiControl(False)
+
